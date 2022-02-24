37b5ab3ef505380e6d7094908cfca8e33985c685
도용주 <qud6330@naver.com>
2022-02-24 11:45:38 +0900

    시각화와 if문 통합

--- ModelSolution.py
+++ ModelSolution.py
@@ -24,7 +24,7 @@ class Cell(BehaviorModelExecutor):
         self.init_state("IDLE")
         self.insert_state("IDLE", Infinite)
         self.insert_state("MOVE", 1)
-
+        
         self.ix = ix
         self.iy = iy
 
@@ -44,14 +44,17 @@ class Cell(BehaviorModelExecutor):
 
     def ext_trans(self, port, msg):
         # if port == "east":
-        print("\n")
         print(f"[{self.ix}, {self.iy}][IN]: {datetime.datetime.now()}")
         self.cancel_rescheduling()
         data = msg.retrieve()
-        # print(data)
-        self.cm_list = data[0]
+
+        self.agent = data[0]
+
+        self.cm_list = self.agent.get_instruction()
+
         print(f"Current Location:{self.get_name()}")
-        print("instruction list : ", self.cm_list)
+        print(self.cm_list)
+
         self._cur_state = "MOVE"
 
     def output(self):
@@ -63,96 +66,115 @@ class Cell(BehaviorModelExecutor):
             if (self.get_blocked() == True):  # 만약 장애물이라면
                 # get_blocked() 는 definition.py 에 있음
                 msg = SysMessage(self.get_name(), "west")  # 왔던곳으로 다시 돌아간다.
-                print(f"***The current cell[{self.get_name()}] is blocked.***")
+                self.agent.set_flag('rb')
+                print("***The current cell is blocked.***")
                 self.cm_list.insert(0, self.cm)
-
-                next = input("Go back to prev-location. Input new Command : "
-                             )  # 이전 위치로 돌아왔음을 알려주고, 새로운 방향 입력
-
-                # 명령어 리스트의 맨 앞에 위에서 입력 받은 새로운 방향 명령 추가
-                if next == "F":
-                    self.cm_list.insert(0, next)
-                elif next == "L":
-                    self.cm_list.insert(0, next)
-                elif next == "B":
-                    self.cm_list.insert(0, next)
+                self.agent.ifMove()
+                self.agent.set_flag(None)
 
         elif self.cm == "F":
             msg = SysMessage(self.get_name(), "north")
             if (self.get_blocked() == True):
                 msg = SysMessage(self.get_name(), "south")
-                print(f"***The current cell[{self.get_name()}] is blocked.***")
+                self.agent.set_flag('fb') 
+                print("***The current cell is blocked.***")
                 self.cm_list.insert(0, self.cm)
-
-                next = input("Go back to prev-location. Input new Command : ")
-                if next == "L":
-                    self.cm_list.insert(0, next)
-                elif next == "R":
-                    self.cm_list.insert(0, next)
-                elif next == "B":
-                    self.cm_list.insert(0, next)
+                self.agent.ifMove()
+                self.agent.set_flag(None)
 
         elif self.cm == "L":
             msg = SysMessage(self.get_name(), "west")
             if (self.get_blocked() == True):
                 msg = SysMessage(self.get_name(), "east")
-                print(
-                    f"***The current cell [{self.get_name()}] is blocked.***")
+                self.agent.set_flag('lb')
+                print("***The current cell is blocked.***")
                 self.cm_list.insert(0, self.cm)
+                self.agent.ifMove()
+                self.agent.set_flag(None)
 
-                next = input("Go back to prev-location. Input new Command : ")
-                if next == "F":
-                    self.cm_list.insert(0, next)
-                elif next == "R":
-                    self.cm_list.insert(0, next)
-                elif next == "B":
-                    self.cm_list.insert(0, next)
-
-        elif self.cm == "B":
+        elif self.cm == "D":
             msg = SysMessage(self.get_name(), "south")
             if (self.get_blocked() == True):
                 msg = SysMessage(self.get_name(), "north")
-                print(f"***The current cell[{self.get_name()}] is blocked.***")
+                self.agent.set_flag('db') 
+                print("***The current cell is blocked.***")
                 self.cm_list.insert(0, self.cm)
+                self.agent.ifMove()
+                self.agent.set_flag(None)
 
-                next = input("Go back to prev-location. Input new Command : ")
-                if next == "F":
-                    self.cm_list.insert(0, next)
-                elif next == "R":
-                    self.cm_list.insert(0, next)
-                elif next == "L":
-                    self.cm_list.insert(0, next)
-
-        msg.insert(self.cm_list)
+        msg.insert(self.agent)
         return msg
-
+    
     def int_trans(self):
         if self._cur_state == "MOVE":
             self._cur_state = "IDLE"
         else:
             self._cur_state = "MOVE"
 
-
 class str_to_instruction():  # 문자열을 명령어로
     # 문자열을 해석하여 명령어 리스트를 만들어 이동시킨다.
     def __init__(self):
-        self.list_of_instruction = list()
+        self.cm_list = list()
 
     def MoveR(self):
-        self.list_of_instruction.append('R')
+        self.cm_list.insert(0, 'R')
 
     def MoveL(self):
-        self.list_of_instruction.append('L')
+        self.cm_list.insert(0, 'L')
 
     def MoveF(self):
-        self.list_of_instruction.append('F')
+        self.cm_list.insert(0, 'F')
 
     def MoveD(self):
-        self.list_of_instruction.append('B')
+        self.cm_list.insert(0, 'D')
 
     def get_instruction(self):  # 만들어진 명령어 리스트를 반환한다.
-        return self.list_of_instruction
+        return self.cm_list
 
+class Agent():
+    def __init__(self):
+        self.cm_s = ''
+        self.cm_list = []
+        self.flag = ''
+    
+    def set_ifMove(self, block, move):
+        if block == 'rb':
+            self.set_rbMove = move
+        elif block == 'lb':
+            self.set_lbMove = move
+        elif block == 'fb':
+            self.set_fbMove = move
+        elif block == 'db':
+            self.set_dbMove = move
+
+    def ifMove(self):
+        if self.flag == 'rb':
+            if self.set_rbMove == None:
+                return
+            exec(self.set_rbMove)
+        elif self.flag == 'lb':
+            if self.set_lbMove == None:
+                return
+            exec(self.set_lbMove)
+        elif self.flag == 'fb':
+            if self.set_fbMove == None:
+                return
+            exec(self.set_fbMove)
+        elif self.flag == 'db':
+            if self.set_dbMove == None:
+                return
+            exec(self.set_dbMove)
+        
+    def list_of_instruction(self, s):
+        self.cm_s = s
+        for i in range(4): s.MoveF()
+
+    def get_instruction(self):  # 만들어진 명령어 리스트를 반환한다.
+        self.cm_list = self.cm_s.get_instruction()
+        return self.cm_list
+    
+    def set_flag(self, flag):
+        self.flag = flag
 
 # System Simulator Initialization
 se = SystemSimulator()
@@ -172,6 +194,8 @@ for i in range(height):
     for j in range(width):
         if i == 0 and j == 0:  # 시작점은 장애물 x
             c = Cell(0, Infinite, "", "sname", j, i, False)
+        elif i == 1 and j == 0:
+            c = Cell(0, Infinite, "", "sname", j, i, True)
         else:
             b = random.choice([True, False, False])
             c = Cell(0, Infinite, "", "sname", j, i,
@@ -202,9 +226,6 @@ for i in range(height):
 
 #msg = SysMessage("cell", "")
 #msg.insert(["R", "L", "F", "D", "R", "F"])
-
-se.get_engine("sname").insert_input_port("start")
-se.get_engine("sname").coupling_relation(None, "start", mat[0][0], "west")
 
 #시각화파트
 root = Tk()
@@ -238,12 +259,19 @@ for y in range(len(simple_map[0])):
         #                        fill="blue")
 root.mainloop()
 
+
+A = Agent()
+
 s = str_to_instruction()
 print("명령어 입력 :")
 str = input()
 exec(str)  # 명령어를 입력받아서 파이썬 문법으로 변환
 
-se.get_engine("sname").insert_external_event(
-    "start", s.get_instruction())  # 만들어진 명령어 리스트를 insert
+A.list_of_instruction(s)
 
+se.get_engine("sname").insert_input_port("start")
+se.get_engine("sname").coupling_relation(None, "start", mat[0][0], "west")
+
+se.get_engine("sname").insert_external_event(
+    "start", A)  # 만들어진 명령어 리스트를 insert
 se.get_engine("sname").simulate()
